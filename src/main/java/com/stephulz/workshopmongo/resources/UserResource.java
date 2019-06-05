e687434ce12f9484faa9947588085803c993c647
Stefan Sanches <stefansanches9@gmail.com>
2019-06-05 15:22:45 -0300

    C:/Program Files/Git/users FindById, Swagger, Exception Handler

--- src/main/java/com/stephulz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/stephulz/workshopmongo/resources/UserResource.java
@@ -1,17 +1,20 @@
 package com.stephulz.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stephulz.workshopmongo.domain.User;
-import com.stephulz.workshopmongo.service.UserService;
+import com.stephulz.workshopmongo.dto.UserDTO;
+import com.stephulz.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,10 +26,19 @@ public class UserResource {
 	private static final Logger log = LoggerFactory.getLogger(UserResource.class);
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		log.info("LOGGER - RESOURCE - Getting all Users");
-		List<User> users = userService.findAll();
-		return ResponseEntity.ok().body(users);
+		List<User> list = userService.findAll();
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		log.info("LOGGER - RESOURCE - Getting User by ID: " + id);
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 
 }
--- src/main/java/com/stephulz/workshopmongo/services/UserService.java
+++ src/main/java/com/stephulz/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
-package com.stephulz.workshopmongo.service;
+package com.stephulz.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.stephulz.workshopmongo.domain.User;
 import com.stephulz.workshopmongo.repository.UserRepository;
 import com.stephulz.workshopmongo.resources.UserResource;
+import com.stephulz.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -22,6 +24,12 @@ public class UserService {
 	public List<User> findAll() {
 		log.info("LOGGER - SERVICE - Getting all Users ");
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		log.info("LOGGER - SERVICE - Getting User by ID: " + id);
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
