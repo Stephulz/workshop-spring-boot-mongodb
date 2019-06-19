02397f75f7fd5c03a041b81c0c9c307a3e55aa8a
Aluga.com <Aluga.com@DESKTOP-8D40FKO>
2019-06-19 09:16:12 -0300

    Saving posts to the post owner

--- src/main/java/com/stephulz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/stephulz/workshopmongo/config/Instantiation.java
@@ -50,6 +50,11 @@ public class Instantiation implements CommandLineRunner {
 		
 		log.info("LOGGER - CONFIG - Saving instantiated Posts");
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		log.info("LOGGER - CONFIG - Saving posts to the post owner");
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
+		
 	}
 
 }
