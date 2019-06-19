2a8ec9a6277b54538f64cedaf9118c1d97837e36
Aluga.com <Aluga.com@DESKTOP-8D40FKO>
2019-06-19 10:55:45 -0300

    Simple query using query methods

--- src/main/java/com/stephulz/workshopmongo/resources/PostResource.java
+++ src/main/java/com/stephulz/workshopmongo/resources/PostResource.java
@@ -1,5 +1,9 @@
 package com.stephulz.workshopmongo.resources;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +11,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stephulz.workshopmongo.domain.Post;
+import com.stephulz.workshopmongo.resources.util.URL;
 import com.stephulz.workshopmongo.services.PostService;
 
 @RestController
@@ -27,5 +33,14 @@ public class PostResource {
 		log.info("LOGGER - POST RESOURCE - Getting Post by ID: " + id);
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@Valid @RequestParam(value = "text", defaultValue = "") String text) {
+		log.info("LOGGER - POST RESOURCE - Search Posts by Title");
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/stephulz/workshopmongo/services/PostService.java
+++ src/main/java/com/stephulz/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.stephulz.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -21,8 +22,13 @@ public class PostService {
 	private static final Logger log = LoggerFactory.getLogger(UserResource.class);
 
 	public Post findById(String id) {
-		log.info("LOGGER - USER SERVICE - Getting User by ID: " + id);
+		log.info("LOGGER - POST SERVICE - Getting Post by ID: " + id);
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		log.info("LOGGER - POST SERVICE - Search Posts by Title");
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
