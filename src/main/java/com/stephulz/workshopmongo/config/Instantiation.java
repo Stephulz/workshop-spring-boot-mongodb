458426254c0dcb7efeaa1d4ae31dfbd62279fe08
Aluga.com <Aluga.com@DESKTOP-8D40FKO>
2019-06-19 08:50:32 -0300

    Author data projection using DTO

--- src/main/java/com/stephulz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/stephulz/workshopmongo/config/Instantiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.stephulz.workshopmongo.domain.Post;
 import com.stephulz.workshopmongo.domain.User;
+import com.stephulz.workshopmongo.dto.AuthorDTO;
 import com.stephulz.workshopmongo.repository.PostRepository;
 import com.stephulz.workshopmongo.repository.UserRepository;
 
@@ -41,11 +42,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Going for a trip", "I'm going to São Paulo, see ya!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "Today i woke up happy!", maria);
-				
 		log.info("LOGGER - CONFIG - Saving instantiated Users");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Going for a trip", "I'm going to São Paulo, see ya!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good morning", "Today i woke up happy!", new AuthorDTO(maria));
 		
 		log.info("LOGGER - CONFIG - Saving instantiated Posts");
 		postRepository.saveAll(Arrays.asList(post1, post2));
